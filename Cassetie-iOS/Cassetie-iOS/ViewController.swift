042fe9b2758354d0c6129a1ee1c86ac668bd7a81
SojinLee <513sojin@naver.com>
2023-01-09 14:18:15 +0900

    [Feat] #1 - 프로젝트 세팅

---
+++ Cassetie-iOS/Cassetie-iOS/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Cassetie-iOS
+//
+//  Created by Sojin Lee on 2023/01/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
