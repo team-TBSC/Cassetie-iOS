10c51a8780d8502027debffbf24950724fd45bfa
513sojin <513sojin@naver.com>
2023-02-22 15:35:09 +0900

    [Add] #2 - 이미지 및 extension 추가

---
+++ Cassetie/Cassetie-iOS/Resource/Image.swift
@@ -0,0 +1,13 @@
+//
+//  Image.swift
+//  Cassetie-iOS
+//
+//  Created by Sojin Lee on 2023/02/22.
+//
+
+import UIKit.UIImage
+
+enum Image {
+    static let backgroundImg = UIImage(named: "background")!
+    static let icSearch = UIImage(named: "search-normal")!
+}
