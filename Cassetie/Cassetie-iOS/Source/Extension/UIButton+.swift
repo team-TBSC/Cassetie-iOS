3a04c394016f5fa6c6d5fa56f040ab766f9044e9
513sojin <513sojin@naver.com>
2023-04-26 22:28:39 +0900

    [Feat] #20 - UIButton extension 추가

---
+++ Cassetie/Cassetie-iOS/Source/Extension/UIButton+.swift
@@ -0,0 +1,20 @@
+//
+//  UIButton+.swift
+//  Cassetie-iOS
+//
+//  Created by Sojin Lee on 2023/04/26.
+//
+
+import UIKit
+
+extension UIButton {
+    func setUnderline() {
+        guard let title = title(for: .normal) else { return }
+        let attributedString = NSMutableAttributedString(string: title)
+        attributedString.addAttribute(.underlineStyle,
+                                      value: NSUnderlineStyle.single.rawValue,
+                                      range: NSRange(location: 0, length: title.count)
+        )
+        setAttributedTitle(attributedString, for: .normal)
+    }
+}
